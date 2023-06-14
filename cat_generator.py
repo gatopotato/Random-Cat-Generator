3b3e0a45068ab6cddeb4a5227d995578f5997f68
Samkit Jaina <samkitjai09@gmail.com>
2023-06-14 12:05:51 +0530

    Completed Project

---
+++ cat_generator.py
@@ -0,0 +1,50 @@
+import requests
+import json 
+from PIL import Image
+from io import BytesIO
+
+def jprint(obj):
+    text=json.dumps(obj,sort_keys =  True, indent=4)
+    print(text)
+
+def url():
+    base_url = "https://cataas.com/cat"
+    print("Random Cat Generator")
+    print("1.Will return a random cat")
+    print("2.Will return a random cat with a :tag")
+    print("3.Will return a random gif cat")
+    print("4.Will return a random cat saying :text")
+    print("5.Will return a random cat with a :tag and saying :text")
+    print("6.Will return a random cat saying :text with text's :size and text's :color")
+    choice = int(input("Please enter your choice: "))
+
+    match choice:
+        case 1:
+            ext = ""
+        case 2:
+            tag = input("Input tag: ")
+            ext="/"+tag
+        case 3:
+            ext= "/gif"
+        case 4:
+            text = input("Enter text: ")
+            ext = "/says/" + text
+        case 5:
+            tag = input("Input tag: ")
+            text = input("Enter text: ")
+            ext = "/"+tag+"/says/"+text
+        case _:
+            print("Invalid Choice. Try Again")
+            url()
+    return base_url+ext
+
+final_url = url()
+response = requests.get(final_url)
+if response.status_code == 200:
+    print("Displaying Content")
+    # print(response.content())
+    img = Image.open(BytesIO(response.content))
+    img.show()
+else:
+    print("Error ", end = " ")
+    print(response.status_code)
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Random Cat Generator
+This is a simple programprogram that uses basic cataas(cat as a service) api to generate cat images and gifs with specific tag and text.
